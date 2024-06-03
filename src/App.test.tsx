d79ae8ab7e14c576052282bf778add3fc16efabf
Paweł Kornas <kornasnorge@gmail.com>
2024-06-03 16:20:23 +0200

    app.test fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,69 +1,27 @@
 import App from "./App";
-import { fireEvent, render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe("Initial screen", () => {
-  beforeEach(() => {
-    render(<App />);
-  });
+describe("App", () => {
   test("Should have my ideas board heading", () => {
+    render(<App />);
     const heading = screen.queryByText(/My ideas board/i);
     expect(heading).toBeInTheDocument();
   });
-  test("Should render a button", () => {
-    // const { container } = render(<App />);
-    // const button = container.querySelector("button");
-    // expect(button).toBeInTheDocument();
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-  });
-});
-describe("Opening form button", () => {
-  beforeEach(() => {
+  test("Should display modal with a form inside when button clicked and submit it by clicking Add idea button", async () => {
     render(<App />);
-  });
-  test("Should have onClick event that fires when button clicked", () => {
+    const user = userEvent.setup();
     const button = screen.getByRole("button");
-    const handleClick = vi.fn();
-    expect(button.onclick).not.toBe(null);
-    button.onclick = handleClick;
-    fireEvent.click(button);
-    expect(handleClick).toHaveBeenCalled();
-  });
-  test("Should display modal with a form inside when button clicked", () => {
-    const button = screen.getByRole("button");
-    fireEvent.click(button);
-    const modal = screen.getByRole("dialog");
-    expect(modal).toBeInTheDocument();
-    within(modal).getByRole("form");
-  });
-});
-describe("Modal", () => {
-  beforeEach(() => {
-    render(<App />);
-    const button = screen.getByRole("button");
-    fireEvent.click(button);
-  });
-  test("Should have title and description fields", () => {
-    // https://testing-library.com/docs/queries/bylabeltext
-    const titleInputNode = screen.getByLabelText("Title");
-    const descriptionInputNode = screen.getByLabelText("Description");
-    expect(titleInputNode).toBeInTheDocument();
-    expect(descriptionInputNode).toBeInTheDocument();
-  });
-  test("Should have submitting button", () => {
-    // https://testing-library.com/docs/queries/bylabeltext
-    const submitButton = document.querySelector('button[type="submit"]');
-    expect(submitButton).toBeInTheDocument();
-  });
-  test("Should display submit button when title and description fields are filled with some text", () => {
-    const titleInputNode = screen.getByLabelText("Title");
-    const descriptionInputNode = screen.getByLabelText("Description");
-    // inputting text into inputs
-    fireEvent.change(titleInputNode, { target: { value: "Test Title" } });
-    fireEvent.change(descriptionInputNode, {
-      target: { value: "Test Description" },
-    });
-    const submitButton = document.querySelector('button[type="submit"]');
-    expect(submitButton).not.toBeDisabled();
+    // open form modal
+    await user.click(button);
+    // fill out form
+    await user.type(screen.getByLabelText("Title"), "Test Title");
+    await user.type(screen.getByLabelText("Description"), "Test Description");
+    // submit form
+    const submitButton = screen.getByRole("button", { name: "Add idea" });
+    await user.click(submitButton);
+    // assert that modal is closed after submitting
+    const modal = screen.queryByRole("dialog");
+    expect(modal).not.toBeInTheDocument();
   });
 });
